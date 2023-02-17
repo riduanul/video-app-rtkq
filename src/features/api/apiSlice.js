c10c144ea97605b0de2758c6740ce19cb31d41e8
Riduanul <riduanul.haque1@gmail.com>
2023-02-17 23:06:01 +0600

    backend link added

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,7 +4,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:9000',
+        baseUrl: 'https://video-app-rtkq-server.onrender.com',
     }),
     endpoints:(builder) => ({
         getVideos: builder.query({
